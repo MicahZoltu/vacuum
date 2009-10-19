361c82972b4dba01a263cdc5a5caac809afc63d9
Micah Caldwell <micah@zoltu.net>
2009-10-19 14:24:52 -0700

    * Removed unused imports in HUDActivity.

--- src/com/zoltu/Vacuum/HUDActivity.java
+++ src/com/zoltu/Vacuum/HUDActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.os.HandlerThread;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.zoltu.Vacuum.Messages.GamePause;
 import com.zoltu.Vacuum.Messages.Level;
